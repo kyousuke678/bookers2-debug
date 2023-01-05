6f3819c44d81e1a6181cb83305a84666ad7183b2
kyousuke678 <119562080+kyousuke678@users.noreply.github.com>
2023-01-05 11:18:44 +0000

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about"=>"homes#about",as: "about"
   root to: "homes#top"
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:index,:show,:edit,:update]
 end
